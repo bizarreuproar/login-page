931ae41cacae36499a9b4ba600fc581de7a67331
bizarreuproar <emirys774@gmail.com>
2024-01-31 15:25:20 +0300

    Refactor login page layout and remove unused code

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import Logo from '../logo.svg';
@@ -22,49 +21,46 @@ const onSubmit = (values) => {
 
 const Login = () => {
 
-  const [nameFocused, setNameFocused] = useState(false);
-  const [passwordFocused, setPasswordFocused] = useState(false);
-
-
-
   return (
     <div className='bg-layoutColor flex items-center justify-center h-screen w-full'>
-      <div className='shadow-lg w-[70%] bg-white rounded-md lg:w-[25%] h-fit flex flex-col items-center md:p-12 p-[20px]'>
-        <div className='flex justify-center mt-5' ><img src={Logo.src} alt="logo_image"></img></div>
+      <div className='shadow-lg w-[70%] bg-white rounded-md lg:w-[25%] h-fit flex flex-col items-center lg:p-8 px-[20px] py-10'>
+        <div className='flex justify-center mt-5 items-center gap-2' ><img src={Logo.src} alt="logo_image"></img><span className='font-bold text-xl'>sneat</span></div>
         <div className=' mx-6 mt-10 mb-5 w-full'>
           <h1 className='font-bold text-2xl  text-[#5B6C7C] '>Hoşgeldin!</h1>
-          <p className='text-text'>Kullanıcı adını ve şifreni girerek devam edebilirsin</p>
+          <p className='text-darkGray'>Kullanıcı adını ve şifreni girerek devam edebilirsin</p>
         </div>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
         >
+          {props => (
           <Form className='flex flex-col gap-4 w-full'>
-            <div className={` relative ${nameFocused ? 'border-primary' : ''}`}>
-              <label htmlFor="name" className={`absolute left-4 top-3 text-lightGray transition-all duration-200 ${nameFocused ? 'top-[-6px] text-xs text-primary bg-white' : 'text-base'}`} >Kullanıcı Adı</label>
-              <Field className={`w-full border rounded-md h-[50px] px-4 focus:outline-none focus:ring-0 ${nameFocused ? 'border-primary' : 'border-darkGray'}`} onFocus={() => setNameFocused(true)} onBlur={() => setNameFocused(false)} type="text" id="name" name="name" />
-              <ErrorMessage name="name" component="div" />
+            <div className='relative group'>
+                <label htmlFor="name" className={`absolute left-4 top-3 text-lightGray group-focus-within:top-[-6px] group-focus-within:px-1 group-focus-within:left-[13px] group-focus-within:text-xs group-focus-within:text-primary group-focus-within:bg-white transition-all duration-200 ${props.values.name.length >= 1 ? 'top-[-6px] text-xs px-1 left-[13px]  bg-white' : 'text-base'}`} >Kullanıcı Adı</label>
+                <Field className='w-full text-lightGray border rounded-md h-[50px] px-4 border-darkGray focus:border-primary focus:outline-none focus:ring-0' type="text" id="name" name="name" />
+                <ErrorMessage name="name" component="div" />
             </div>
             <div>
 
-              <div className='relative'>
-                <label htmlFor="password" className={`absolute top-3 left-4 text-lightGray transition-all duration-200 ${passwordFocused ? 'top-[-6px] text-xs text-primary bg-white' : 'text-base'}`}>Şifre</label>
-                <Field className={`w-full border rounded-md h-[50px] border-darkGray px-4 focus:outline-none focus:ring-0 ${passwordFocused ? 'border-primary' : 'border-darkGray'}`} type="password" id="password" name="password" onFocus={() => setPasswordFocused(true)} onBlur={() => setPasswordFocused(false)} />
+              <div className='relative group'>
+                <label htmlFor="password" className={`absolute top-3 left-4 text-lightGray group-focus-within:top-[-6px] group-focus-within:text-xs group-focus-within:left-[13px] group-focus-within:px-1 group-focus-within:text-primary group-focus-within:bg-white transition-all duration-200 ${props.values.password.length >= 1 ? 'top-[-6px] left-[13px]  text-xs px-1 bg-white' : 'text-base'}`}>Şifre</label>
+                <Field className='w-full border rounded-md h-[50px] text-lightGray border-darkGray px-4 focus:border-primary focus:outline-none focus:ring-0' type="password" id="password" name="password" />
                 <ErrorMessage name="password" component="div" />
               </div>
-
+              
               <div className='relative flex justify-between items-center py-2'>
                 <div className='flex items-center gap-2'>
-                  <Field className='border w-4 h-4 rounded-md border-lightGray ' type="checkbox" id="rememberMe" name="rememberMe" />
+                  <Field className='border w-4 h-4 rounded-md border-darkGray ' type="checkbox" id="rememberMe" name="rememberMe" />
                   <label className=' text-darkGray cursor-pointer' htmlFor='rememberMe'>Beni Hatırla</label>
                 </div>
                 <p className='text-darkGray cursor-pointer'>Şifremi unuttum?</p>
               </div>
             </div>
 
-            <button className=' w-full rounded-md h-[40px]  bg-primary text-white px-4 hover:bg-[#696CFF]/95 transition duration-150' type="submit">Giriş Yap</button>
+            <button className=' w-full rounded-md h-[40px]  bg-primary text-white px-4 hover:bg-primary/95 transition duration-150' type="submit">Giriş Yap</button>
           </Form>
+          )}
         </Formik>
       </div>
     </div>
